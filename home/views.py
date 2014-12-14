fd41aba300ce6df2746168e3c1332e29a345adac
bboy.stats <bboy.stats@gmail.com>
2014-12-14 15:41:41 +0100

    add person object

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from base.person.models import Person
 
 
 def home(request, username=None, template_name='home.html'):
@@ -10,6 +11,10 @@ def home(request, username=None, template_name='home.html'):
         home
             Home object list
     """
+
+    persons = Person.objects.all()
+
     return render(request, template_name, {
+        'persons': persons,
         'title': 'Total Feeling Crew',
     })
