3e9808a4410d59ef15f9da2c71c4852befbe5a12
bboy.stats <bboy.stats@gmail.com>
2014-12-17 14:32:00 +0100

    add medias

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from base.person.models import Person
+from base.media.models import Media
 
 
 def home(request, username=None, template_name='home.html'):
@@ -11,10 +12,11 @@ def home(request, username=None, template_name='home.html'):
         home
             Home object list
     """
-
+    medias = Media.objects.all().order_by('-created')[:4]
     persons = Person.objects.all()
 
     return render(request, template_name, {
         'persons': persons,
+        'object_list': medias,
         'title': 'Total Feeling Crew',
     })
