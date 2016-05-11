11fa5af3c9804a100b338ffc69ed13c4d99e9ede
Gilles <gilles@mappy.com>
2016-05-11 11:38:24 +0200

    upgrade to https

--- site_media/static/js/media.js
+++ site_media/static/js/media.js
@@ -19,15 +19,15 @@ app.controller("Media", function($scope, $http, Reddit, $sce) {
           yy: "%d years ago"
       }
   });
-  $scope.reddit = new Reddit('http://bboyrankingz.com/media/search/total%20feeling%20crew/.json');
+  $scope.reddit = new Reddit('https://bboyrankingz.com/media/search/total%20feeling%20crew/.json');
 
-  $http.get('http://bboyrankingz.com/crews/members/total-feeling-crew/.json').
+  $http.get('https://bboyrankingz.com/crews/members/total-feeling-crew/.json').
     success(function(data, status, headers, config) {
       $scope.members = data["results"];
     });
 
  $scope.open = function(id) {
-    $http.get('http://bboyrankingz.com/media/embed/' + id).
+    $http.get('https://bboyrankingz.com/media/embed/' + id).
     success(function(data, status, headers, config) {
       $scope.embed = $sce.trustAsHtml(data);
     });
@@ -35,7 +35,7 @@ app.controller("Media", function($scope, $http, Reddit, $sce) {
 
   $scope.show_member = function(slug) {
     console.log(slug)
-    $scope.reddit = new Reddit('http://bboyrankingz.com/media/search/' + slug + '/.json');
+    $scope.reddit = new Reddit('https://bboyrankingz.com/media/search/' + slug + '/.json');
     $scope.reddit.nextPage();
     
   };
